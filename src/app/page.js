c365bbaedcc08c9faa0dbf2e3723a10321d9154c
KrimsonJosh <jumoinzad@gmail.com>
2024-08-01 21:22:47 -0500

    added inventory collection from firestore and useEffect to update inventory

--- src/app/page.js
+++ src/app/page.js
@@ -32,7 +32,21 @@ export default function Home() {
   const [inventory, setInventory] = useState([])
   const [open, setOpen] = useState(false)
   const [itemName, setItemName] = useState('')
-  // We'll add our component logic here
+
+  const updateInventory = async () => {
+    const snapshot = query(collection(firestore, 'inventory'))
+    const docs = await getDocs(snapshot)
+    const inventoryList = []
+    docs.forEach((doc) => {
+      inventoryList.push({ name: doc.id, ...doc.data() })
+    })
+    setInventory(inventoryList)
+  }
+  
+  useEffect(() => {
+    updateInventory()
+  }, [])
+  
   return (
     <Box>
       <Typography variant="h1">Inventory Management</Typography>
